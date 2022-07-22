e3a6d7f0e7913b0e4faedc63a7451f5977665c6b
KetanC1211 <ketanchindarkar12@gmail.com>
2022-07-22 19:42:56 +0530

    addlastmethod addded

--- src/com/bridgelabz/Main.java
+++ src/com/bridgelabz/Main.java
@@ -5,8 +5,8 @@ public class Main {
 	public static void main(String[] args) {
 		LinkedList list = new LinkedList();
 		list.addFirst(56);
-		list.addFirst(30);
-		list.addFirst(70);
+		list.addLast(30);
+		list.addLast(70);
 		list.display();
 	}
 }
