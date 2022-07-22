8b1a0f2d8fd762cc25f5859b172d87db1021294c
KetanC1211 <ketanchindarkar12@gmail.com>
2022-07-22 19:57:10 +0530

    Refactored UC3

--- src/com/bridgelabz/LinkedList.java
+++ src/com/bridgelabz/LinkedList.java
@@ -13,6 +13,11 @@ public class LinkedList {
         public Node(int value) {
             this.value = value;
         }
+        //Used for inserting value at particular index.
+        public Node(int value, Node next) { 
+            this.value = value;
+            this.next = next;
+        }
     }
     
     //initial size Zero
@@ -54,5 +59,25 @@ public class LinkedList {
         size++;
     }
 
+    //Adding Value at given index by using insertBetween method    
+    public void insertValAtIndex(int val, int index) {
+        if (index == 0) {
+            addFirst(val);
+            return;
+        }
+        if (index == size) {
+            addLast(val);
+            return;
+        }
+
+        Node temp = head;
+        for (int i = 1; i < index; i++) {
+            temp = temp.next;
+        }
+
+        Node node = new Node(val, temp.next);
+        temp.next = node;
+        size++;
+    }
     
 }
